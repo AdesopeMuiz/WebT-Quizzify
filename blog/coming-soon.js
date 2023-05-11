91375154a7e20994991634870839d91348a0d933
AdesopeMuiz <104017196+AdesopeMuiz@users.noreply.github.com>
2023-05-11 11:14:49 +0100

    Add files via upload

---
+++ blog/coming-soon.js
@@ -0,0 +1,7 @@
+// Notify me button click event
+var notifyButton = document.querySelector(".button");
+notifyButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  alert("Thank you for your interest! We will notify you when we launch.");
+});
+
